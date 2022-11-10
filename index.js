5451e7ff5d41d1b9fd4fc373a87e8f02270d193b
Bashir Dol (dolb) <dolb@coventry.ac.uk>
2022-11-10 19:16:02 +0000

    posts rendered from javascript

--- index.js
+++ index.js
@@ -28,3 +28,49 @@ const posts = [
     }
 ]
 
+const sectionEl = document.getElementById("main-section")
+
+
+              
+            
+function addToMain(){
+    
+    let htmlToGoInSection = ""     
+    
+    for(let i = 0 ; i < posts.length ; i++){
+    htmlToGoInSection += ` 
+                
+              <div class="container">  
+                <div class="user-info">
+                    <img src="${posts[i].avatar}" class="profile-avatar" id="avatar">
+                    <div class="user-details">
+                        <p><span class="bold" id="name">${posts[i].name}</span></p>
+                        <p id="location">${posts[i].location}</p>
+                    </div>    
+                </div>
+                   
+                <img src="${posts[i].post}" class="post" id="post" >
+                
+                <div class="post-info">
+                  <div class="post-btns">
+                        <button class="like-btn"></button>
+                        <button class="comment-btn"></button>
+                        <button class="dm-btn"></button>
+                  </div> 
+                    <p class="likes"><span class="bold"> <span id="likes">${posts[i].likes}</span>,495 Likes</span></p>
+                    <p class="comment"><span class="bold" id="username"> ${posts[i].username} </span><span id="comment">${posts[i].comment}</span></p>   
+                </div>  
+              <div>  
+                
+               `
+               
+               
+    }
+    
+    sectionEl.innerHTML = htmlToGoInSection
+    
+}    
+
+addToMain()        
+
+
